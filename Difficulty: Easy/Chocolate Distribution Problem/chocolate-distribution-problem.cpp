90f6e58a996a10cc6e73276fa10ad86b4986a25a
Sayan Chakraborty <107755575+Sayan4391@users.noreply.github.com>
2024-07-21 00:00:51 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Easy/Chocolate Distribution Problem/chocolate-distribution-problem.cpp
@@ -0,0 +1,47 @@
+//{ Driver Code Starts
+#include <bits/stdc++.h>
+using namespace std;
+
+// } Driver Code Ends
+class Solution{
+    public:
+    long long findMinDiff(vector<long long> a, long long n, long long m){
+    //code
+       sort(a.begin(),a.end());
+      long long mini=INT_MAX;
+      int l=0;
+      int r=m-1;
+      while(r<n){
+           long long temp=a[r]-a[l];
+           mini=min(temp,mini);
+           r++;
+           l++;
+      }
+      return mini;
+    }   
+};
+
+//{ Driver Code Starts.
+int main() {
+	long long t;
+	cin>>t;
+	while(t--)
+	{
+		long long n;
+		cin>>n;
+		vector<long long> a;
+		long long x;
+		for(long long i=0;i<n;i++)
+		{
+			cin>>x;
+			a.push_back(x);
+		}
+		
+		long long m;
+		cin>>m;
+		Solution ob;
+		cout<<ob.findMinDiff(a,n,m)<<endl;
+	}
+	return 0;
+}
+// } Driver Code Ends
